a0cc518cf231c302737bb598b865c3fa586e75be
Étienne Charron <93718161+etennecharron@users.noreply.github.com>
2023-11-05 16:32:42 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 let hero = document.querySelector(".masthead");
+let contact = document.getElementById("contact");
 hero.style.backgroundImage = "none";
 hero.style.backgroundColor = "black";
+contact.style.backgroundImage = "none";
+contact.style.backgroundColor = "black";
 
